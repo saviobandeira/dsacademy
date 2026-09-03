232606c65c8af2bbc7b869c44e406ec8df91ff0f
saviobandeira <savio-bandeira@hotmail.com>
2026-09-03 19:03:01 -0300

    feat: add Atividade entity

---
+++ src/main/java/com/devsuperior/dsacademy/Entities/Atividade.java
@@ -0,0 +1,72 @@
+package com.devsuperior.dsacademy.Entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String descricao;
+    private Double price;
+
+    public Atividade() {
+    }
+
+    public Atividade(Integer id, String name, String descricao, Double price) {
+        this.id = id;
+        this.name = name;
+        this.descricao = descricao;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Atividade atividade = (Atividade) o;
+        return Objects.equals(id, atividade.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
